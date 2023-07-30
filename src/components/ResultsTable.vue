6920f1ed34cb7ed9b7ebebd90278a6c62d3a61f6
Lenni009 <guentherhanslphdyt@gmail.com>
2023-07-30 20:17:21 +0200

    table header; more obj transforms

--- src/components/ResultsTable.vue
+++ src/components/ResultsTable.vue
@@ -4,7 +4,7 @@ import { storeToRefs } from 'pinia';
 import { computed } from 'vue';
 
 const filterStore = useFilterStore();
-const { data: filteredData } = storeToRefs(filterStore);
+const { filteredData } = storeToRefs(filterStore);
 
 interface TextArray {
   text: string;
@@ -33,19 +33,40 @@ const dataArray = computed(() => {
   const textArray: TextArray[] = [];
   for (const data of filteredData.value) {
     for (const [key, value] of Object.entries(data)) {
-      if (key === 'UnixTimestamp') continue;
+      if (key === 'Timestamp') continue;
+
       let text: string = '';
       let className: string = '';
-      if (key === 'Platform') {
-        text = getFullPlatform(value as string);
-      } else {
-        text = value.toString(); // NoSonar this won't be [object Object]
-        if (key === 'Glyphs') className = 'glyphs';
+
+      switch (key) {
+        case 'Name':
+          text = (value as string) || 'Unknown (procedural name)';
+          if (!value) className = 'italic';
+          break;
+
+        case 'Platform':
+          text = getFullPlatform(value as string);
+          break;
+
+        case 'UnixTimestamp':
+          text = new Date(value as number).toLocaleDateString();
+          break;
+
+        case 'Correctly Tagged':
+          text = (value as boolean).toString().charAt(0).toUpperCase() + (value as boolean).toString().slice(1);
+          break;
+
+        default:
+          text = value.toString(); // NoSonar this won't be [object Object]
+          if (key === 'Glyphs') className = 'glyphs';
+          break;
       }
+
       const newObj = {
         text,
         className,
       };
+
       textArray.push(newObj);
     }
     if (textArray.length === 60) break;
@@ -55,7 +76,16 @@ const dataArray = computed(() => {
 </script>
 
 <template>
-  <div class="data-table">
+  <div
+    v-if="dataArray.length"
+    class="data-table"
+  >
+    <div class="header">System Name</div>
+    <div class="header">Glyphs</div>
+    <div class="header">Discoverer</div>
+    <div class="header">Platform</div>
+    <div class="header">Date</div>
+    <div class="header">Tagged</div>
     <div
       v-for="text in dataArray"
       :class="text.className"
@@ -71,5 +101,19 @@ const dataArray = computed(() => {
   grid-template-columns: repeat(6, auto);
   column-gap: 0.5rem;
   align-items: center;
+
+  .header {
+    font-weight: bold;
+    border-block-end: 1px solid var(--color);
+    padding-block-end: 0.25rem;
+    padding-block-start: 0.5rem;
+    position: sticky;
+    top: 0;
+    background-color: var(--background-color);
+  }
+
+  .italic {
+    font-style: italic;
+  }
 }
 </style>
