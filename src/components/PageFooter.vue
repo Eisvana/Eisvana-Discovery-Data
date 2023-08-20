7666f93e938c4024e1171e2a75793c40f09c23fc
Lenni009 <guentherhanslphdyt@gmail.com>
2023-08-20 13:22:18 +0200

    add note about data age

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,14 +1,18 @@
 <template>
   <footer class="footer">
-    <p class="credits">
-      Project enabled by the research of the
-      <a
-        href="http://nmscd.com/Atlas-Insight-Initiative/"
-        target="_blank"
-        rel="noopener noreferrer"
-        >Atlas Insight Initiative</a
-      >
-    </p>
+    <div class="credits">
+      <p>Data last updated on 1 August 2023</p>
+      <p>
+        Project enabled by the research of the
+
+        <a
+          href="http://nmscd.com/Atlas-Insight-Initiative/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Atlas Insight Initiative</a
+        >
+      </p>
+    </div>
   </footer>
 </template>
 
@@ -23,6 +27,8 @@
 }
 
 .credits {
-  font-size: initial;
+  & > p {
+    font-size: initial;
+  }
 }
 </style>
