4c478b9ca9584e1165f0790272b74eda76c7d4d2
Lenni009 <guentherhanslphdyt@gmail.com>
2024-05-23 20:32:26 +0200

    issue was closed -> remove workaround

--- src/components/DetailsWrapper.vue
+++ src/components/DetailsWrapper.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 
 defineProps<{
   summary: string;
@@ -11,14 +11,10 @@ function changeOpenState(e: Event) {
   if (!(e.target instanceof HTMLDetailsElement)) return;
   isOpen.value = Boolean(e.target.open);
 }
-
-// temporary workaround, see https://github.com/vuejs/language-tools/issues/4373
-const details = ref<HTMLDetailsElement | null>(null);
-onMounted(() => details.value?.addEventListener('toggle', changeOpenState));
 </script>
 
 <template>
-  <details ref="details">
+  <details @toggle="changeOpenState">
     <summary>{{ summary }}</summary>
     <slot v-if="isOpen"></slot>
   </details>
