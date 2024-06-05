5f6cc5abc6f435d8f1616ced96b14484c3d03a8b
Lenni009 <guentherhanslphdyt@gmail.com>
2024-06-05 22:22:34 +0200

    use breadcrumb navigation when on system or planet page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
 import ThemeSwitch from './ThemeSwitch.vue';
+import { computed } from 'vue';
+
+const route = useRoute();
+const glyphs = computed(() => route.params.glyphs);
+const isPlanet = computed(() => route.name?.toString().includes('planetOverview'));
 </script>
 
 <template>
-  <nav>
+  <nav aria-label="Main navigation">
     <ul>
-      <li><a href="/">&larr; View other pages</a></li>
+      <li v-if="!glyphs"><a href="/">&larr; View other pages</a></li>
+      <li v-else>
+        <nav aria-label="breadcrumb">
+          <ul>
+            <li><RouterLink to="/">Table</RouterLink></li>
+            <template v-if="isPlanet">
+              <li><RouterLink :to="`/system/${glyphs}`">System</RouterLink></li>
+              <li>Planet</li>
+            </template>
+            <li v-else>System</li>
+          </ul>
+        </nav>
+      </li>
     </ul>
     <ul>
       <li>
