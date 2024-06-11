76a5da011a5b17e28cd0ffc9a47481ce0735bb52
Lenni009 <guentherhanslphdyt@gmail.com>
2024-06-11 15:28:21 +0200

    minor styling adjustments

--- src/components/DiscoveryNote.vue
+++ src/components/DiscoveryNote.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="text-caption q-mb-sm">
-    Note: The numbers on this page represent the amount of <span class="text-italic">discovered</span> items. This number may
-    be less than what is actually present.
+  <div class="text-caption q-mb-sm discovery-note">
+    Note: The numbers on this page represent the amount of <span class="text-italic">discovered</span> items. This
+    number may be less than what is actually present.
   </div>
 </template>
+
+<style scoped lang="scss">
+.discovery-note {
+  letter-spacing: unset;
+}
+</style>
