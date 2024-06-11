8af0859cfcc4773ec8ae2f4ad9cc0390804d80c0
Lenni009 <guentherhanslphdyt@gmail.com>
2024-06-11 18:31:08 +0200

    Somewhat working table

--- src/pages/ResultsTable.vue
+++ src/pages/ResultsTable.vue
@@ -1,48 +1,159 @@
 <script setup lang="ts">
-import TableHeaders from '@/components/table/TableHeaders.vue';
-import { useDataStore } from '@/stores/data';
-import type { DiscoveryData, TableHeadings } from '@/types/data';
-import { storeToRefs } from 'pinia';
-import { ref } from 'vue';
 import { getFormattedUTCDateString } from '@/helpers/date';
-import Pagination from '@/components/table/Pagination.vue';
 import { getPlatform } from '@/helpers/platform';
-import TableSkeleton from '@/components/table/TableSkeleton.vue';
+import { useDataStore } from '@/stores/data';
+import type { DiscoveryData } from '@/types/data';
+import type { Platform } from '@/types/platform';
+import { storeToRefs } from 'pinia';
+import { computed, ref, reactive } from 'vue';
+import type { QTableProps } from 'quasar';
 
 const dataStore = useDataStore();
-const { filteredData } = storeToRefs(dataStore);
-
-const paginatedData = ref<DiscoveryData[]>([]);
+const { filteredData, isLoading } = storeToRefs(dataStore);
 
 const capitaliseFirst = (str: string) => `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
 
-const headers: TableHeadings = {
-  normal: ['System Name', 'Glyphs', 'Discoverer', 'Platform', 'Date', 'Tagged'],
-};
+const pagination = ref();
 
-const updateData = (newPaginatedArray: DiscoveryData[]) => (paginatedData.value = newPaginatedArray);
+const requiredCols = ref(['name', 'glyphs', 'discoverer', 'platform', 'date']);
 
-// const columns = [
-//   {
+const discovererColName = computed(() => {
+  const isBase = (item: DiscoveryData) => 'Parts' in item;
 
-//   }
-// ]
+  const allHaveBuilder = filteredData.value.some(isBase);
+  if (allHaveBuilder) return 'Builder';
+
+  const someHaveBuilder = filteredData.value.every(isBase);
+  if (someHaveBuilder) return 'Discoverer / Builder';
+
+  return 'Discoverer';
+});
+
+const columns = reactive([
+  {
+    name: 'name',
+    label: 'Name',
+    align: 'left',
+    field: 'Name',
+    format: (name: string) => name || 'Unknown (procedural name)',
+    classes: (row: DiscoveryData) => (row.Name ? '' : 'italic'),
+  },
+  {
+    name: 'glyphs',
+    label: 'Glyphs',
+    field: 'Glyphs',
+    align: 'left',
+    classes: 'glyphs glyphs-cell',
+  },
+  {
+    name: 'discoverer',
+    label: discovererColName,
+    align: 'left',
+    field: 'Discoverer',
+  },
+  {
+    name: 'platform',
+    label: 'Platform',
+    align: 'left',
+    field: 'Platform',
+    format: (val: Platform) => getPlatform(val),
+  },
+  {
+    name: 'date',
+    label: 'Date',
+    align: 'left',
+    field: 'Timestamp',
+    format: (val: number) => getFormattedUTCDateString(val),
+  },
+  {
+    name: 'prefixed',
+    label: 'Correctly Prefixed',
+    field: 'Correctly Prefixed',
+    align: 'left',
+    sortable: true,
+    format: (val: boolean) => capitaliseFirst(val.toString()),
+  },
+  {
+    name: 'fauna',
+    label: 'Fauna',
+    field: 'Fauna',
+    align: 'left',
+    sortable: true,
+  },
+  {
+    name: 'flora',
+    label: 'Flora',
+    field: 'Flora',
+    align: 'left',
+    sortable: true,
+  },
+  {
+    name: 'minerals',
+    label: 'Minerals',
+    field: 'Minerals',
+    align: 'left',
+    sortable: true,
+  },
+  {
+    name: 'settlements',
+    label: 'Settlements',
+    field: 'Settlements',
+    align: 'left',
+    sortable: true,
+  },
+  {
+    name: 'bases',
+    label: 'Bases',
+    field: 'Bases',
+    align: 'left',
+    sortable: true,
+  },
+  {
+    name: 'mode',
+    label: 'Gamemode',
+    field: 'Gamemode',
+    align: 'left',
+    sortable: true,
+  },
+  {
+    name: 'parts',
+    label: 'Parts',
+    field: 'Parts',
+    align: 'left',
+    sortable: true,
+  },
+]);
+
+const rowsPerPage = [10, 50, 100, 500, 1000, 0]; // NoSonar these show how many items are displayed on the page at once. The user can switch between them
 </script>
 
 <template>
-  <div
-    v-if="filteredData.length"
-    class="table-wrapper"
-  >
-    <Pagination
-      :data="filteredData"
-      @change="updateData"
-    />
+  <div class="table-wrapper">
+    <QTable
+      :columns
+      :loading="isLoading"
+      :rows="filteredData"
+      :rows-per-page-options="rowsPerPage"
+      :visible-columns="requiredCols"
+      class="my-sticky-header-table"
+      row-key="glyphs"
+      flat
+      v-model:pagination="pagination"
+    >
+      <template v-slot:body-cell-name="props">
+        <QTd :props="props">
+          <RouterLink :to="`/system/${props.row.Glyphs}`">{{
+            props.row.Name || 'Unknown (procedural name)'
+          }}</RouterLink>
+        </QTd>
+      </template>
+    </QTable>
+    <!--
     <div class="data-table">
       <TableHeaders :headers />
       <template v-for="obj in paginatedData">
         <div :class="{ italic: !obj.Name }">
-          <RouterLink :to="`/system/${obj.Glyphs}`">{{ obj.Name || 'Unknown (procedural name)' }}</RouterLink>
+
         </div>
         <div class="glyphs">
           {{ obj.Glyphs }}
@@ -60,21 +171,12 @@ const updateData = (newPaginatedArray: DiscoveryData[]) => (paginatedData.value 
           {{ capitaliseFirst(obj['Correctly Prefixed']?.toString() ?? '') }}
         </div>
       </template>
-    </div>
+    </div> -->
   </div>
-  <TableSkeleton v-else />
 </template>
 
 <style scoped lang="scss">
-.table-wrapper {
-  display: flex;
-  flex-direction: column;
-
-  .data-table {
-    display: grid;
-    grid-template-columns: repeat(6, auto);
-    column-gap: 0.5rem;
-    align-items: center;
-  }
+:deep(.glyphs-cell) {
+  font-size: 2rem;
 }
 </style>
