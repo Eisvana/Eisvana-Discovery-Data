0cc81aa79a5737e2abcec7a775bdf136bac615d2
Lenni009 <guentherhanslphdyt@gmail.com>
2024-06-13 18:02:38 +0200

    use built-in router props for breadcrumbs

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,31 +5,57 @@ import { computed } from 'vue';
 const route = useRoute();
 const glyphs = computed(() => route.params.glyphs);
 const isPlanet = computed(() => route.name?.toString() === 'planetOverview');
+
+const eisvanaHosts = ['atlas.eisvana.com', 'data.eisvana.com'];
+const isEisvanaHost = eisvanaHosts.includes(window.location.host);
 </script>
 
 <template>
-  <nav aria-label="Main navigation">
-    <a
+  <nav>
+    <QItem
       v-if="!glyphs"
-      href="/"
-      >&larr; View other pages</a
+      :href="isEisvanaHost ? 'https://eisvana.com' : '/'"
     >
-    <QBreadcrumbs
-      v-else
-      separator=">"
-    >
-      <QBreadcrumbsEl> <RouterLink to="/">Table</RouterLink></QBreadcrumbsEl>
-      <template v-if="isPlanet">
-        <QBreadcrumbsEl><RouterLink :to="`/system/${glyphs}`">System</RouterLink></QBreadcrumbsEl>
-        <QBreadcrumbsEl>Planet</QBreadcrumbsEl>
-      </template>
-      <QBreadcrumbsEl v-else>System</QBreadcrumbsEl>
-    </QBreadcrumbs>
+      <QItemSection>
+        <QIcon name="arrow_back" />
+      </QItemSection>
+      <QItemSection avatar>View other pages</QItemSection>
+    </QItem>
+    <QItem v-else>
+      <QBreadcrumbs
+        active-color="white"
+        class="breadcrumbs-wrapper"
+        separator=">"
+      >
+        <QBreadcrumbsEl
+          class="link"
+          label="Table"
+          to="/"
+          exact
+        />
+        <QBreadcrumbsEl
+          v-if="isPlanet"
+          :to="`/system/${glyphs}`"
+          class="link"
+          label="System"
+          exact
+        />
+
+        <QBreadcrumbsEl
+          :label="isPlanet ? 'Planet' : 'System'"
+          class="text-grey-4"
+        />
+      </QBreadcrumbs>
+    </QItem>
   </nav>
 </template>
 
 <style scoped lang="scss">
-.breadcrumb-nav li {
-  padding-block: 0;
+.link {
+  text-decoration: underline;
+}
+
+.breadcrumbs-wrapper {
+  align-self: center;
 }
 </style>
