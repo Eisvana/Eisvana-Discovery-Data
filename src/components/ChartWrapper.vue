12ea271c6bb747be5ab8d2aa5b7f268750cffdba
Lenni009 <guentherhanslphdyt@gmail.com>
2024-06-16 16:19:35 +0200

    use Quasar components for wrapper

---
+++ src/components/ChartWrapper.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{
+  summary: string;
+}>();
+
+// using different properties for them because v-model sets its values before show and before hide
+const isOpen = ref(false);
+const isFullWidth = ref(false);
+</script>
+
+<template>
+  <QExpansionItem
+    v-model="isFullWidth"
+    :class="{ 'full-width': isFullWidth }"
+    :label="summary"
+    @after-hide="isOpen = false"
+    @before-show="isOpen = true"
+  >
+    <div
+      v-if="isOpen"
+      class="q-px-md"
+    >
+      <slot></slot>
+    </div>
+  </QExpansionItem>
+</template>
