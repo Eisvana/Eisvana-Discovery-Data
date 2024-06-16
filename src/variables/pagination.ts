745c46c036f1298a51768d4e6f28f41aea772ec6
Lenni009 <guentherhanslphdyt@gmail.com>
2024-06-16 14:17:51 +0200

    remove higher settings of items per page

--- src/variables/pagination.ts
+++ src/variables/pagination.ts
@@ -1,1 +1,1 @@
-export const rowsPerPage = [10, 50, 100, 500, 1000, 0]; // NoSonar these show how many items are displayed on the page at once. The user can switch between them
+export const rowsPerPage = [10, 50, 100]; // NoSonar these show how many items are displayed on the page at once. The user can switch between them
