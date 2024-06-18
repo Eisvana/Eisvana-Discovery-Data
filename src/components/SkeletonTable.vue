c60c198729fb57e0d6550e816cc254ef6ebdad47
Lenni009 <guentherhanslphdyt@gmail.com>
2024-06-18 10:13:48 +0200

    add skeleton table component

---
+++ src/components/SkeletonTable.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface Props {
+  amountOfHeaders?: number;
+  dataLength?: number;
+}
+
+withDefaults(defineProps<Props>(), {
+  amountOfHeaders: 6,
+  dataLength: 5,
+});
+</script>
+
+<template>
+  <QMarkupTable>
+    <thead>
+      <tr>
+        <th
+          v-for="_n in amountOfHeaders"
+          scope="col"
+        >
+          <QSkeleton type="text" />
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="_n in dataLength">
+        <td v-for="_m in amountOfHeaders">
+          <QSkeleton type="text" />
+        </td>
+      </tr>
+    </tbody>
+  </QMarkupTable>
+</template>
