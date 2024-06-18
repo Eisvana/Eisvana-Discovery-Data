3ef3c24e6e40b48fd9330bf32e7da21f63838dbc
Lenni009 <guentherhanslphdyt@gmail.com>
2024-06-18 14:45:59 +0200

    simplify type

--- src/types/worker.ts
+++ src/types/worker.ts
@@ -2,7 +2,7 @@ import type { FilterStoreState } from '@/stores/filter';
 import type { DiscoveryData } from './data';
 import type { UnixTimestamp } from './date';
 
-export type FilterConfig = Omit<Omit<FilterStoreState, 'date'>, 'categories'> & { unixTimestamp: UnixTimestamp };
+export type FilterConfig = Omit<FilterStoreState, 'date' | 'categories'> & { unixTimestamp: UnixTimestamp };
 
 export interface WorkerMessage {
   regions: string[];
