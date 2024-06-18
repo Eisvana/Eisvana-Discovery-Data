e5336a4c49c7441eda868b8c97574f5153a4e321
Lenni009 <guentherhanslphdyt@gmail.com>
2024-06-18 10:13:58 +0200

    add loading skeletons

--- src/pages/DiscoveryStats.vue
+++ src/pages/DiscoveryStats.vue
@@ -4,18 +4,38 @@ import PlatformStats from '@/components/stats/PlatformStats.vue';
 import NumberStats from '@/components/stats/NumberStats.vue';
 import { useDataStore } from '@/stores/data';
 import { storeToRefs } from 'pinia';
+import SkeletonTable from '@/components/SkeletonTable.vue';
 
 const dataStore = useDataStore();
-const { dataLength } = storeToRefs(dataStore);
+const { dataLength, isLoading } = storeToRefs(dataStore);
+
+const amountOfStatRows = 5;
+const amountOfStatItems = amountOfStatRows * 2;
 </script>
 
 <template>
   <div
-    v-if="dataLength"
+    :class="{ 'q-px-md q-gutter-y-xl': isLoading }"
     class="column q-gutter-y-lg"
   >
-    <NumberStats />
-    <DiscovererStats />
-    <PlatformStats />
+    <template v-if="!isLoading">
+      <template v-if="dataLength">
+        <NumberStats />
+        <DiscovererStats />
+        <PlatformStats />
+      </template>
+    </template>
+
+    <template v-else>
+      <div class="number-stats-wrapper">
+        <QSkeleton
+          v-for="_n in amountOfStatItems"
+          width="200px"
+          type="text"
+        />
+      </div>
+      <SkeletonTable />
+      <SkeletonTable />
+    </template>
   </div>
 </template>
