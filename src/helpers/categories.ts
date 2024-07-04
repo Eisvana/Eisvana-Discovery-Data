219531ea9ed17ec9318c225b0f77f2db71fca5d6
Lenni009 <guentherhanslphdyt@gmail.com>
2024-07-04 13:24:28 +0300

    use index find function

--- src/helpers/categories.ts
+++ src/helpers/categories.ts
@@ -2,4 +2,4 @@ import type { DiscoveryCategories } from '@/types/data';
 import { availableCategories } from '@/variables/categories';
 
 export const isValidCategory = (cat: unknown): cat is DiscoveryCategories =>
-  availableCategories.find((item) => item === cat) !== undefined;
+  availableCategories.findIndex((item) => item === cat) !== -1;
