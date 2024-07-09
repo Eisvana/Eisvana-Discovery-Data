161ae4cec260d370dfd329b88e53e4e61ce372e5
Lenni009 <guentherhanslphdyt@gmail.com>
2024-07-09 14:34:22 +0200

    move data loading back to app component

---
+++ src/types/events.ts
@@ -0,0 +1,1 @@
+export type GlobalCustomEvents = 'filter' | 'reset';
---
+++ src/variables/events.ts
@@ -0,0 +1,3 @@
+import type { GlobalCustomEvents } from '@/types/events';
+
+export const globalCustomEvents: GlobalCustomEvents[] = ['filter', 'reset'];
