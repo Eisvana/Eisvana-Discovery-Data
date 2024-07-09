c994d5f79617626369085ce059ce7fb47a5873be
Lenni009 <guentherhanslphdyt@gmail.com>
2024-07-09 15:34:21 +0200

    add comment

--- src/helpers/typeGuards.ts
+++ src/helpers/typeGuards.ts
@@ -7,6 +7,7 @@ export const isPlatformCode = (code: unknown): code is Platform =>
 
 export const isObject = (item: unknown): item is object => Boolean(item) && typeof item === 'object';
 
+// this runs in a web worker and must not use any quasar dependencies, that's why we're using our own isObject() function here
 export function isDiscoveryData(item: unknown): item is DiscoveryData {
   if (!isObject(item)) return false;
   return 'Name' in item && 'Glyphs' in item && 'Discoverer' in item && 'Platform' in item && 'Timestamp' in item;
