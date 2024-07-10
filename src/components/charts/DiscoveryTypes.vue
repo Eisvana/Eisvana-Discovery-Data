1041add931faf329ba23726b8b57b852688c41f4
Lenni009 <guentherhanslphdyt@gmail.com>
2024-07-10 09:48:26 +0200

    use user given labels

--- src/components/charts/DiscoveryTypes.vue
+++ src/components/charts/DiscoveryTypes.vue
@@ -4,38 +4,41 @@ import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, Li
 import { useDataStore } from '@/stores/data';
 import { storeToRefs } from 'pinia';
 import { computed } from 'vue';
-import { refDebounced } from '@vueuse/core';
+import { computedWithControl, refDebounced } from '@vueuse/core';
 import { debounceDelay } from '@/variables/debounce';
 import { chartOptions } from '@/variables/chart';
 import { categoryMapping } from '@/variables/mappings';
 import type { DiscoveryCategories, DiscoveryData } from '@/types/data';
 import PieChartWrapper from '../PieChartWrapper.vue';
+import { useFilterStore } from '@/stores/filter';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement);
 
 const dataStore = useDataStore();
 const { filteredData, isLoading } = storeToRefs(dataStore);
 
+const filterStore = useFilterStore();
+const { sortedCategories } = storeToRefs(filterStore);
+
 const debouncedFilteredData = refDebounced<DiscoveryData[]>(filteredData, debounceDelay);
 
-const categoryData = computed(() => {
-  const labels = new Set<string>();
-  const colours = new Set<string>();
+const categoryData = computedWithControl(debouncedFilteredData, () => {
+  const labels = sortedCategories.value;
+  const colours = labels.map((cat) => categoryMapping[cat].colour);
+
   const categoryCounters: Partial<Record<DiscoveryCategories, number>> = {};
+  labels.forEach((cat) => (categoryCounters[cat] = 0));
 
   // prettier-ignore
   for (let i = 0; i < debouncedFilteredData.value.length; i++) {  // NoSonar this is for performance
     const item = debouncedFilteredData.value[i];
-    const { Category } = item;
-    labels.add(categoryMapping[Category].label);
-    colours.add(categoryMapping[Category].colour);
-    categoryCounters[Category] ??= 0;
-    categoryCounters[Category]++;
+    const category = item.Category;
+    if (categoryCounters[category] !== undefined) categoryCounters[category]++;
   }
 
   return {
-    colours: Array.from(colours),
-    labels: Array.from(labels),
+    colours,
+    labels,
     data: Object.values(categoryCounters),
   };
 });
