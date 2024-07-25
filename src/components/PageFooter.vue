857c842c2387730eee05c433cd0ac88eedf1e5b1
Lenni009 <guentherhanslphdyt@gmail.com>
2024-07-25 10:00:31 +0200

    update data

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="credits q-pb-md">
-    <p>Data last updated on 22 July 2024</p>
+    <p>Data last updated on 25 July 2024</p>
     <p>
       Project enabled by the research of the
       <a
